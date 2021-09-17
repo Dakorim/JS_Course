7c39ce364760f20ba49bcb6ae4c6c1b7ce38628f
Cabrera Bruno Nicolas <cabrerabruno22@gmail.com>
2021-09-17 20:29:20 -0300

    Add files via upload

---
+++ Evaluacion2_Cabrera_Bruno_Nicolás/js/recetario.js
@@ -0,0 +1,7 @@
+
+var ensalada1=prompt("Prepare una buena ensalda con: ");
+        document.write(ensalada1)
+        var ensalada2=prompt("Le agregamos: ");
+        document.write(", " + ensalada2) 
+        var ensalada3=prompt("La finalizamos con: ");
+        document.write(" y por ultimo " + ensalada3)
